4a57154d01315929803e7c120eb7eff14da975cc
Selyss <99344963+Selyss@users.noreply.github.com>
2024-02-27 22:15:14 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -16,17 +16,14 @@ function App() {
         direction="horizontal"
         className="min-h-[200px] max-w-md rounded-lg border"
       >
-        <ResizablePanel defaultSize={25}>
+        <ResizablePanel>
           <div className="flex h-full items-center justify-center p-6">
-            <span className="font-semibold">Sidebar</span>
             <Sidebar />
           </div>
         </ResizablePanel>
         <ResizableHandle withHandle />
-        <ResizablePanel defaultSize={75}>
-          <div className="flex h-full items-center justify-center p-6">
-            <span className="font-semibold">Content</span>
-          </div>
+        <ResizablePanel>
+          <div className="flex h-full items-center justify-center p-6"></div>
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
