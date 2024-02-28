8c080e0d49df9eef4746d000a9daf17c17b56f71
Selyss <99344963+Selyss@users.noreply.github.com>
2024-02-27 19:15:33 -0500

    some components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "../src/components/ui/resizeable";
 import "./output.css";
 
 function App() {
-  return <h1 className="text-blue-400">hello</h1>;
+  return (
+    <ResizablePanelGroup
+      direction="horizontal"
+      className="min-h-[200px] max-w-md rounded-lg border"
+    >
+      <ResizablePanel defaultSize={25}>
+        <div className="flex h-full items-center justify-center p-6">
+          <span className="font-semibold">Sidebar</span>
+        </div>
+      </ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel defaultSize={75}>
+        <div className="flex h-full items-center justify-center p-6">
+          <span className="font-semibold">Content</span>
+        </div>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
 }
 
 export default App;
