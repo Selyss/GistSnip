e571837a5fecac452f503e452dd085683f655f1f
Selyss <99344963+Selyss@users.noreply.github.com>
2024-02-27 21:03:33 -0500

    dark mode and more components

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,33 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "../src/components/ui/resizeable";
+import { ModeToggle } from "./components/ui/dark-mode";
+import { Sidebar } from "./components/ui/nav";
 import "./output.css";
 
 function App() {
   return (
-    <ResizablePanelGroup
-      direction="horizontal"
-      className="min-h-[200px] max-w-md rounded-lg border"
-    >
-      <ResizablePanel defaultSize={25}>
-        <div className="flex h-full items-center justify-center p-6">
-          <span className="font-semibold">Sidebar</span>
-        </div>
-      </ResizablePanel>
-      <ResizableHandle withHandle />
-      <ResizablePanel defaultSize={75}>
-        <div className="flex h-full items-center justify-center p-6">
-          <span className="font-semibold">Content</span>
-        </div>
-      </ResizablePanel>
-    </ResizablePanelGroup>
+    // HACK:
+    <div>
+      <ModeToggle />
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="min-h-[200px] max-w-md rounded-lg border"
+      >
+        <ResizablePanel defaultSize={25}>
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold">Sidebar</span>
+            <Sidebar />
+          </div>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={75}>
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold">Content</span>
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
   );
 }
 
